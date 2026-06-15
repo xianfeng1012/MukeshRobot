9bec1d1e0e39377ccb95793d9573a51c52266ede
xianfeng1012 <xianfeng1989@gmail.com>
2026-06-15 23:21:35 +0800

    i18n: 汉化 dicegame.py

--- MukeshRobot/modules/dicegame.py
+++ MukeshRobot/modules/dicegame.py
@@ -10,42 +10,42 @@ from pyrogram.handlers import MessageHandler
 async def dice(bot, message):
     x=await bot.send_dice(message.chat.id)
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-  
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
+
 @mukesh.on_message(filters.command("dart"))
 async def dart(bot, message):
     x=await bot.send_dice(message.chat.id, "🎯")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
 
 @mukesh.on_message(filters.command("basket"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🏀")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
 @mukesh.on_message(filters.command("jackpot"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎰")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
 @mukesh.on_message(filters.command("ball"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎳")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
 @mukesh.on_message(filters.command("football"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "⚽")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"{message.from_user.mention} 你的得分是：{m}",quote=True)
 __help__ = """
- Play Game With Emojis:
-/dice - Dice 🎲
-/dart - Dart 🎯
-/basket - Basket Ball 🏀
-/ball - Bowling Ball 🎳
-/football - Football ⚽
-/jackpot - Spin slot machine 🎰
+ 用表情玩游戏：
+/dice - 掷骰子 🎲
+/dart - 飞镖 🎯
+/basket - 篮球 🏀
+/ball - 保龄球 🎳
+/football - 足球 ⚽
+/jackpot - 老虎机 🎰
  """
 
-__mod_name__ = "Dɪᴄᴇ"
+__mod_name__ = "骰子游戏"

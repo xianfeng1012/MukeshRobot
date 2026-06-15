915b65cceb5c50add88c3ab3f3c9979da3f1875c
xianfeng1012 <xianfeng1989@gmail.com>
2026-06-15 23:01:14 +0800

    i18n: 汉化 urlshortner.py

--- MukeshRobot/modules/urlshortner.py
+++ MukeshRobot/modules/urlshortner.py
@@ -15,7 +15,7 @@ async def short_urls(bot, message):
     await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/short [url]`")
+            "**示例：**\n\n`/short [链接]`")
 #     url_pattern = re.compile(r'https?://\S+')
     link=message.command[1]
 #     link = url_pattern.findall(urls)
@@ -42,17 +42,17 @@ async def short_urls(bot, message):
          ikb("Clckru Url",url=clckru_link)
         ]
         ]
-        await message.reply_text(f"Here are few shortened links :",reply_markup=ikm(url))
+        await message.reply_text(f"以下是几个短链接：",reply_markup=ikm(url))
 
     except Exception as e:
-        await message.reply_text(f"Either the link is already shortened or is invalid.")
+        await message.reply_text(f"该链接可能已经是短链接或无效链接。")
 
 @mukesh.on_message(filters.command(["unshort"]))
 async def unshort(bot, message):
     await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/unshort [short - url]`")
+            "**示例：**\n\n`/unshort [短链接]`")
     link=message.text.split(' ')[1]
     
     try:
@@ -71,18 +71,18 @@ async def unshort(bot, message):
 
         
 
-        await message.reply_text(f"Here's the unshortened link :\n`{x}` " ,reply_markup=ikm(url))
+        await message.reply_text(f"以下是还原后的原始链接：\n`{x}` " ,reply_markup=ikm(url))
 
         
 
     except Exception as e:
 
-        await message.reply_text(f"ᴇʀʀᴏʀ:    {e} ")
+        await message.reply_text(f"错误：{e} ")
 # mukesh.add_handler(MessageHandler(short_urls))
 # mukesh.add_handler(MessageHandler(unshort))
 __help__ = """
-ᴍᴀᴋᴇ sʜᴏʀᴛs ᴏғ ᴀ ɢɪᴠᴇɴ ʟɪɴᴋ 
- ❍ /short <url>  *:Example `/short https://t.me/mr_sukkun`.
+缩短给定的链接
+ ❍ /short <链接> *：示例 `/short https://t.me/mr_sukkun`。
  *"""
 
-__mod_name__ = "Sʜᴏʀᴛᴇɴᴇʀ"
+__mod_name__ = "短链接"

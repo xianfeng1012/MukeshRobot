c2ee7eac265430500c37ab7b13e1840a0fa51927
xianfeng1012 <xianfeng1989@gmail.com>
2026-06-16 00:06:28 +0800

    i18n: 汉化 revel.py

--- MukeshRobot/modules/revel.py
+++ MukeshRobot/modules/revel.py
@@ -28,22 +28,22 @@ async def get_vars(_, message: Message):
     try:
         await app.send_message(
             chat_id=int(OWNER_ID),
-            text=f"""<u>**{BOT_NAME} ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs :**</u>
+            text=f"""<u>**{BOT_NAME} 配置变量：**</u>
 
-**ʙᴏᴛ_ᴛᴏᴋᴇɴ :** `{TOKEN}`
-**sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ :** `{SUPPORT_CHAT}`
-**Sᴛᴀʀᴛ Iᴍᴀɢᴇ :** `{START_IMG}`
-**Aᴘɪ Iᴅ :** `{API_ID}`
-**Aᴘɪ Hᴀsʜ :** `{API_HASH}` 
-**Mᴏɴɢᴏ Uʀʟ :** `{MONGO_DB_URI}`   
+**机器人令牌：** `{TOKEN}`
+**支持群组：** `{SUPPORT_CHAT}`
+**启动图片：** `{START_IMG}`
+**API ID：** `{API_ID}`
+**API Hash：** `{API_HASH}`
+**MongoDB 地址：** `{MONGO_DB_URI}`
 
 
 
 
 """)
     except:
-        return await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs.")
+        return await message.reply_text("» 发送配置变量失败。")
     if message.chat.type != ChatType.PRIVATE:
         await message.reply_text(
-            "» ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘᴍ, ɪ'ᴠᴇ sᴇɴᴛ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs ᴛʜᴇʀᴇ."
+            "» 请查看您的私信，我已将配置变量发送至那里。"
         )

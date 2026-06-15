183d45a0befea63d63e94793fa6864e4de721769
xianfeng1012 <xianfeng1989@gmail.com>
2026-06-15 23:53:52 +0800

    i18n: 汉化 gps.py

--- MukeshRobot/modules/gps.py
+++ MukeshRobot/modules/gps.py
@@ -11,7 +11,7 @@ async def gps(bot, message):
 #     await message.delete()
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/gps [latitude , longitude]`")
+            "**示例：**\n\n`/gps [纬度 , 经度]`")
     x = message.text.split(' ')[1].split(',')
    
 
@@ -37,7 +37,7 @@ async def gps(bot, message):
 
             [IKB
 
-             ("Open with:🌏ɢᴏᴏɢʟᴇ ᴍᴀᴘs ",url=f"https://www.google.com/maps/search/{latitude},{longitude}")
+             ("用谷歌地图打开 🌏",url=f"https://www.google.com/maps/search/{latitude},{longitude}")
 
             ]
 
@@ -46,13 +46,13 @@ async def gps(bot, message):
     #     await message.reply_text(f"{gm}")
         await message.reply_venue(latitude, longitude,f"{city}",f"{state} ,{country}",reply_markup=IKM(url))
     except Exception as e:
-        await message.reply_text(f"I can't find that \nDue to {e}")
+        await message.reply_text(f"无法找到该位置 \n错误：{e}")
 @mukesh.on_message(filters.command(["distance"]))
 async def distance(bot, message):
     await message.delete()
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Example:**\n\n`/distance [latitude , longitude],[latitude , longitude]`")
+            "**示例：**\n\n`/distance [纬度 , 经度],[纬度 , 经度]`")
 
     x = message.text.split(" ")[1].split(',',2)[0:2]
     y = message.text.split(" ")[1].split(',',4)[2:4]
@@ -68,19 +68,19 @@ async def distance(bot, message):
         """
         distance=(great_circle(x,y).miles)
 
-        await message.reply_text(f"Total distance between {x[0]},{x[1]} and {y[0]},{y[1]} is {distance}")
-        
+        await message.reply_text(f"{x[0]},{x[1]} 与 {y[0]},{y[1]} 之间的总距离为 {distance} 英里")
+
     except Exception as e:
-        await message.reply_text(f"I can't find that \nDue to {e}")
+        await message.reply_text(f"无法计算距离 \n错误：{e}")
         
 # mukesh.add_handler(MessageHandler(gps))     
 # mukesh.add_handler(MessageHandler(distance))
 
 __help__ = """
-sᴇɴᴅs ʏᴏᴜ ᴛʜᴇ ɢᴘs ʟᴏᴄᴀᴛɪᴏɴ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ǫᴜᴇʀʏ...
+发送指定查询的 GPS 位置信息……
 
- ❍ /gps <ʟᴏᴄᴀᴛɪᴏɴ>*:* ɢᴇᴛ ɢᴘs ʟᴏᴄᴀᴛɪᴏɴ.
- ❍ /distance  to measure distance 
+ ❍ /gps <坐标>*:* 获取 GPS 位置。
+ ❍ /distance  测量两点之间的距离
 """
 
-__mod_name__ = "Gᴘs"
+__mod_name__ = "GPS 定位"
